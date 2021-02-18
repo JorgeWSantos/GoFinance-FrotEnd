1157cad0d1344d5a2ac2d88107261b56d50a97d6
Jorge <jorgews.dev@gmail.com>
2021-02-18 20:27:45 -0300

    challenge done ! 🚀

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,15 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+// const formatDateValue = (value: string): string => {
+//   const year = value.substring(0, 4);
+//   const month = value.substring(5, 7);
+//   const day = value.substring(8, 10);
+
+//   return `${day}/${month}/${year}`;
+// };
 
 export default formatValue;
